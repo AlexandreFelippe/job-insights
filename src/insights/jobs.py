652566afb3ff225721e8ab6a1feb92d2e6b8d00e
AlexandreFelippe <alemfelippe@gmail.com>
2024-02-26 17:52:35 -0300

    Refaz requisito 03 e Entregua requisito 4

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,13 +13,17 @@ class ProcessJobs:
                 self.jobs_list.append(row)
 
     def get_unique_job_types(self) -> List[str]:
-        # A função deve retornar uma lista de valores únicos presentes na coluna job_type
         unique_job_types = list()
         for job in self.jobs_list:
             if job["job_type"] not in unique_job_types:
                 unique_job_types.append(job["job_type"])
-
         return unique_job_types
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, jobs, filters) -> List[dict]:
+        filtered_jobs = []
+        if not type(filters) is dict:
+            raise TypeError
+        for job in jobs:
+            if all(job.get(key) == value for key, value in filters.items()):
+                filtered_jobs.append(job)
+        return filtered_jobs
