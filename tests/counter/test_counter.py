be1a2f82751bbbecf050c283122ac619f1fae6d0
AlexandreFelippe <alemfelippe@gmail.com>
2024-02-26 15:28:46 -0300

    Requisito 01

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+path = "data/jobs.csv"
+word = "Python"
 
 
 def test_counter():
-    raise
+    assert count_ocurrences(path, word) == 1639
